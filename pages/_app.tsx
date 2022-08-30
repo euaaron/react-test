b59fb263330ebeaa21a8f21042fc9d3d0342f292
Aaron Carneiro <github@aaroncarneiro.com>
2022-08-30 12:34:41 -0300

    (develop) Added PWA support (#2)
    * chore: setup jest
    
    * chore: setup cypress e2e and component
    
    * docs: added deployment badge
    
    * chore: added prettier config
    
    * todo: checked deploy
    
    * chore: setup app as PWA
    
    * fix: major lighthouse warnings
    
    + added html=lang
    + added logo svg
    + added meta theme-color
    + removed user-scalable
    + set maximum-scale to 5
    
    * fix: updated yarn.lock
    
    * fix: added lockfiles to gitignore
    
    * fix: removed yarn.lock

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="A PWA made to prove my React skills and made with NextJS to be secure, serverless and cheap to deploy 🙂"
+        />
+        <meta name="keywords" content="react, nextjs, pwa, serverless, test, cypress, jest, ci" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="theme-color" content="#282a36" />
+        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5" />
+
+        <title>React Test</title>
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
