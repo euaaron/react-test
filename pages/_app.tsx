f252ae9c65153f3e284a4f5c3cd6b04651ba2f1e
Aaron Carneiro <github@aaroncarneiro.com>
2022-08-30 12:09:20 -0300

    fix: major lighthouse warnings
    + added html=lang
    + added logo svg
    + added meta theme-color
    + removed user-scalable
    + set maximum-scale to 5

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,14 +6,15 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta charSet='utf-8' />
-        <meta name="description" content="A PWA made to prove my React skills and made with NextJS to be secure, serverless and cheap to deploy 🙂" />
+        <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="A PWA made to prove my React skills and made with NextJS to be secure, serverless and cheap to deploy 🙂"
+        />
         <meta name="keywords" content="react, nextjs, pwa, serverless, test, cypress, jest, ci" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-        />
+        <meta name="theme-color" content="#282a36" />
+        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5" />
 
         <title>React Test</title>
         <link rel="manifest" href="/manifest.json" />
