1b8322456096e16ac5e5c3f6155b7171850e83de
Aaron Carneiro <github@aaroncarneiro.com>
2022-08-30 17:10:58 -0300

    chore: added Roboto font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,11 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-BR">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>          
+          <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+        </Head>
         <body>
           <Main />
           <NextScript />
