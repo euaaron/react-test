83d91c6b39cf1e9657c8645d74666054c42db0ca
Aaron Carneiro <github@aaroncarneiro.com>
2022-08-30 17:18:32 -0300

    feat: created ActiveLink component
    - change link color if href matches current route

---
+++ components/Navbar/ActiveLink/ActiveLink.tsx
@@ -0,0 +1,35 @@
+import { useRouter } from 'next/router'
+import React, { PropsWithChildren, useEffect, useState } from 'react'
+import styles from './ActiveLink.module.scss'
+
+interface ActiveLinkProps {
+  href: string
+}
+
+/**
+ * @description A React Component link that is active if the current path is the same as the href
+ * @param href - the href of the link 
+ * @returns React.FC
+ */
+const ActiveLink: React.FC<PropsWithChildren<ActiveLinkProps>> = ({ children, href }) => {
+  const router = useRouter()
+  const [isActive, setIsActive] = useState(false)
+
+  useEffect(() => {
+    setIsActive(router.pathname === href)
+  }, [href, router])
+
+  function handleClick(e: any) {
+    e.preventDefault()    
+    router.push(href)
+    setIsActive(true)
+  }
+
+  return (
+    <a className={isActive ? styles.activeLink : ''} href={href} onClick={handleClick}>
+      {children}
+    </a>
+  )
+}
+
+export default ActiveLink
